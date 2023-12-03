e6f70fb343ec835ce2a5d67cbce8c883bae353c5
Andy <a.patten@beckhoff.com>
2023-12-03 15:48:09 +1100

    Updated script.

--- scripts/tc3-build-script.ps1
+++ scripts/tc3-build-script.ps1
@@ -1,29 +1,33 @@
 param(
-  [string]$targetNetId
+  [string]$solutionPath,
+  [string]$targetNetId,
+  [string]$targetPlatform
 )
 
 # Print received parameters for logging
 Write-Host "tc3-build-script Running"
 Write-Host "Received parameters:"
 
+Write-Host "Target NetId: $solutionPath"
 Write-Host "Target NetId: $targetNetId"
+Write-Host "Target NetId: $targetPlatform"
 
 
 # Try-Catch block for error handling
 try {
-    # Your logic here...
+  # Your logic here...
 
     
 
-    # Simulating a failure
-    #if ($simulateFail -eq "true") {
-    #  throw "Simulated failure triggered."
-    #}
+  # Simulating a failure
+  #if ($simulateFail -eq "true") {
+  #  throw "Simulated failure triggered."
+  #}
 }
 catch {
-    # Catching and displaying the error
-    Write-Host "Error: $_"
-    exit 1
+  # Catching and displaying the error
+  Write-Host "Error: $_"
+  exit 1
 }
 
 # Successful execution
